3480a1d8ff86c4f4530356149d89ddc29ab23c0a
Bianca Brown <biancakbrown58@gmail.com>
2020-04-01 17:07:43 -0400

    question-answer page css

---
+++ ClientApp/src/pages/AnswerPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Answer from '../components/Answer'
+
+const AnswerPage = () => {
+  return (
+    <>
+      <Answer />
+    </>
+  )
+}
+
+export default AnswerPage
--- ClientApp/src/pages/Home.jsx
+++ ClientApp/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export function Home() {
   return (
     <div>
       <h2>All Questions</h2>
       <button>Ask Question</button>
+      <Link to="/answer">answer</Link>
     </div>
   )
 }
