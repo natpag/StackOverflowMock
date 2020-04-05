c0543c9779f4000a60343c8875aa00f0f57e52f6
Bianca Brown <biancakbrown58@gmail.com>
2020-04-05 12:50:28 -0400

    added answer to db context

--- Models/DatabaseContext.cs
+++ Models/DatabaseContext.cs
@@ -8,7 +8,7 @@ namespace StackOverflowMock.Models
   public partial class DatabaseContext : DbContext
   {
 
-
+    public DbSet<Answer> Answers { get; set; }
 
 
     private string ConvertPostConnectionToConnectionString(string connection)
