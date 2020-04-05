1278d6c1244fae05c3c56b32a6c550f8e5464b22
corinne carew <ccarew@mail.usf.edu>
2020-04-05 13:07:19 -0400

    tried to add functionality to askpage

--- ClientApp/src/pages/AskPage.jsx
+++ ClientApp/src/pages/AskPage.jsx
@@ -1,5 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+import { Redirect } from 'react-router-dom'
 
+// const PostQuestion = () => {
+//   const [question, setQuestion] = useState({})
+//   const [wasSuccessfullyCreated, setWasSuccessfullyCreated] = useState({
+//     shouldRedirect: false,
+//     newTrailInformation: {},
+//   })
+
+//   const addQuestionToApi = async () => {
+//     console.log('adding', question)
+//     const resp = await axios.post('/api/question', question)
+//     if (resp.status === 201) {
+//       // do something something else
+//       setWasSuccessfullyCreated({
+//         shouldRedirect: true,
+//           newQuestionInformation: resp.data, })
+
+const AddQuestion = () => {
+  const [question, setQuestion] = useState({})
+  const [wasSuccessfullyCreated, setWasSuccessfullyCreated] = useState({
+    shouldRedirect: false,
+    newQuestionInformation: {},
+  })
+
+  const updateQuestionData = e => {
+    const key = e.target.name
+    const value = e.target.value
+    setQuestion(prevQuestion => {
+      prevQuestion[key] = value
+      return prevQuestion
+    })
+  }
+
+  const addQuestionToApi = async () => {
+    console.log('adding', question)
+    const resp = await axios.post('/api/questions', question)
+    if (resp.status === 201) {
+      // do something something else
+      setWasSuccessfullyCreated({
+        shouldRedirect: true,
+        newQuestionInformation: resp.data,
+      })
+    } else {
+      // do something else here
+    }
+  }
+
+  if (wasSuccessfullyCreated.shouldRedirect) {
+    return (
+      <Redirect
+        to={{
+          pathname: `/question/${wasSuccessfullyCreated.newQuestionInformation.id}`,
+          state: { trail: wasSuccessfullyCreated.newQuestionInformation },
+        }}
+      />
+    )
+  }
+}
 const AskPage = () => {
   return (
     <>
@@ -14,7 +73,7 @@ const AskPage = () => {
             <h2>Title</h2>
             <input class="Title"></input>
             <h2>Body</h2>
-            <input class="Body"></input>
+            <input class="Body" onChange={updateQuestionData}></input>
             <h2>tags</h2>
             <input class="Tags"></input>
           </section>
@@ -45,7 +104,9 @@ const AskPage = () => {
             </div>
           </section>
         </section>
-        <button className="AskPageButton">submit</button>
+        <button className="AskPageButton" onClick={addQuestionToApi}>
+          submit
+        </button>
       </body>
     </>
   )
